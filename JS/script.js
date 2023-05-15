1f2aa47c507c90198074c929fbf2908ab3d88ee9
ავთანდილ საგინაშვილი <120257059+AvtandilSaginashvili@users.noreply.github.com>
2023-05-15 20:59:59 +0400

    update

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+
+function logout(){
+    var x = document.getElementById("main");
+
+    x.style.display = "none";
+}
+
+function cancel() {
+    var x = document.getElementById("main");
+
+    x.style.display = "block";
+}
